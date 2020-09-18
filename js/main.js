da3b05b4bd9fdc8a754630f575e07598d735d506
kate liashko <kathrin-d@yandex.ru>
2020-09-18 14:58:39 +0300

    section about-us-content slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'  
+});
+
